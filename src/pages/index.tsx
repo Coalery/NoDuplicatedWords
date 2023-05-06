787144d940f75cac6b8e18fa43bf9ac83cd2de79
Coalery <doralife12@naver.com>
2023-05-06 22:19:20 +0900

    feat: 개수 및 랜덤 선택 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,57 @@
-import * as React from "react";
-import type { HeadFC, PageProps } from "gatsby";
+import * as React from 'react';
+import { Chance } from 'chance';
+import type { HeadFC, PageProps } from 'gatsby';
 
 const IndexPage: React.FC<PageProps> = () => {
   const [words, setWords] = React.useState<string[]>([]);
-  const [input, setInput] = React.useState<string>("");
-
-  const onClickCopyAll = () => {
-    const result = words.join("\n");
-    navigator.clipboard.writeText(result);
-  };
+  const [input, setInput] = React.useState<string>('');
+  const [randomOne, setRandomOne] = React.useState<string | null>(null);
 
   const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     const inp = e.target.value;
 
     setInput(inp);
-    setWords(Array.from(new Set(inp.split("\n").filter((v) => !!v))).sort());
+    setWords(
+      Array.from(
+        new Set(
+          inp
+            .split('\n')
+            .flatMap((v) => v.replaceAll(/([a-z])([A-Z])/g, '$1,$2'))
+            .flatMap((v) => v.split(','))
+            .filter((v) => !!v)
+            .map((v) => v.toString())
+        )
+      ).sort()
+    );
+  };
+
+  const onClickCopyAll = () => {
+    const result = words.join('\n');
+    navigator.clipboard.writeText(result);
+  };
+
+  const onClickRandomOne = () => {
+    if (words.length === 0) {
+      return;
+    }
+
+    const chance = new Chance();
+
+    const randomIdx = chance.integer({ min: 0, max: words.length - 1 });
+    const result = words[randomIdx];
+
+    setRandomOne(result);
   };
 
   return (
-    <div style={{ padding: "16px" }}>
+    <div style={{ padding: '16px' }}>
       <div>
         <textarea
           style={{
-            width: "100%",
-            minHeight: "300px",
-            resize: "vertical",
-            boxSizing: "border-box",
+            width: '100%',
+            minHeight: '300px',
+            resize: 'vertical',
+            boxSizing: 'border-box',
           }}
           value={input}
           onChange={onChange}
@@ -34,15 +60,25 @@ const IndexPage: React.FC<PageProps> = () => {
       <div>
         <button
           onClick={onClickCopyAll}
-          style={{ width: "100%", height: "40px" }}
+          style={{ width: '100%', height: '40px' }}
         >
           Copy All
         </button>
       </div>
+      {randomOne && <h3>{randomOne}</h3>}
       <div>
+        <button
+          onClick={onClickRandomOne}
+          style={{ width: '100%', height: '40px' }}
+        >
+          Get Random One
+        </button>
+      </div>
+      <div>
+        <p>Length: {words.length}</p>
         <ul>
-          {words.map((word) => (
-            <li>{word}</li>
+          {words.map((word, idx) => (
+            <li key={idx}>{word}</li>
           ))}
         </ul>
       </div>
